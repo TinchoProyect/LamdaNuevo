450128e0bf47572eab4a672a8366e1c181d78ddd
Martin Ignacio Serrano <miserrano75@gmail.com>
2025-01-29 00:17:48 -0300

    Modificación del encabezado del header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import "./Header.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Lamda } from "../utils/empresaData";
@@ -6,47 +7,33 @@ export const Header = () => {
     return (
         <header className="header-container">
             <div className="container">
-                <div className="header-content">
-                    <div className="header-main">
-                        <div className="logo-section">
-                            <div className="logo-wrapper">
-                                <img
-                                    src="/src/assets/Logo.jpg"
-                                    alt="Logo de Lamda"
-                                    className="logo"
-                                />
-                                <div className="alias-container">
-                                    <span className="info-label">Alias:</span>
-                                    <span className="alias-value">{Lamda.alias}</span>
-                                </div>
-                            </div>
+                <div className="header-main">
+                    {/* Sección Logo - Tamaño original */}
+                    <div className="logo-section">
+                        <div className="logo-wrapper">
+                            <img
+                                src="/src/assets/Logo.jpg"
+                                alt="Lamda Banking"
+                                className="logo"
+                                loading="lazy"
+                            />
                         </div>
                     </div>
-                    <div className="info-grid">
-                        <div className="info-column left">
-                            <div className="info-row">
-                                <span className="info-label">Titular:</span>
-                                <span className="info-value">{Lamda.titular}</span>
-                            </div>
-                            <div className="info-row">
-                                <span className="info-label">DNI:</span>
-                                <span className="info-value">{Lamda.dni}</span>
-                            </div>
-                            <div className="info-row">
-                                <span className="info-label">CUIT:</span>
-                                <span className="info-value">{Lamda.cuit}</span>
-                            </div>
-                        </div>
-                        <div className="info-column right">
-                            <div className="info-row">
-                                <span className="info-label">Tipo de Cuenta:</span>
+
+                    {/* Datos Bancarios - Estructura mejorada */}
+                    <div className="account-info">
+                        <div className="bank-data-container">
+                            <div className="info-item">
+                                <span className="info-label">Cuenta:</span>
                                 <span className="info-value">{Lamda.tipoDeCuenta}</span>
                             </div>
-                            <div className="info-row">
-                                <span className="info-label">Número:</span>
+                            
+                            <div className="info-item">
+                                <span className="info-label">N° Cuenta:</span>
                                 <span className="info-value">{Lamda.numeroDeCuenta}</span>
                             </div>
-                            <div className="info-row">
+                            
+                            <div className="info-item">
                                 <span className="info-label">CBU:</span>
                                 <span className="info-value">{Lamda.cbu}</span>
                             </div>
