73c666fd963605271d03c141b38d21afcc279b7a
Martin Ignacio Serrano <miserrano75@gmail.com>
2025-02-10 04:45:49 -0300

    correccion 4

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,21 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react()],
   server: {
     port: 8081,
     proxy: {
       '/consulta': {
-        target: 'http://1.tcp.sa.ngrok.io:20186', // Asegúrate de que esta URL sea correcta
+        target: 'http://1.tcp.sa.ngrok.io:20186',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/consulta/, '')
       }
     }
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true
   }
 })
