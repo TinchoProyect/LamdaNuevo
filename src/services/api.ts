f8749e4f59b7fb327869d9667474158b12e7b1fb
Martin Ignacio Serrano <miserrano75@gmail.com>
2025-02-25 00:26:01 -0300

    Nueva Https Fija Cloudflare

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,10 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://1.tcp.sa.ngrok.io:20186';
+// URL base anterior (comentada para referencia)
+// const BASE_URL = 'http://1.tcp.sa.ngrok.io:20186';
+
+// URL base actualizada con HTTPS y dominio fijo
+const BASE_URL = 'https://api.lamdaser.com';
 
 const api = axios.create({
     baseURL: BASE_URL,
